dad2110ed5dfc5884c87421098ddf5305b1e37c6
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-10-26 20:17:02 +0200

    added basic notes store structure

--- src/store/notes.js
+++ src/store/notes.js
@@ -4,6 +4,23 @@ export const useNotesStore = defineStore('notes', {
   state: () => ({ notes: [] }),
   getters: {},
   actions: {
-
+    // eslint-disable-next-line arrow-body-style
+    fetchNote: (id) => {
+      // get from server
+      return id;
+    },
+    fetchNotes: () => {
+      // get from server
+    },
+    createNote: (note) => {
+      // send to server
+      this.notes.push(note);
+    },
+    updateNote: (note, id) => {
+      // send to server
+    },
+    deleteNote: (id) => {
+      // send request to server
+    },
   },
 });
