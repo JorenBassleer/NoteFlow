8b9262e60a4608efccab11d1447ed1b53d015035
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-11-04 15:36:10 +0100

    added files

---
+++ src/components/notes/NoteList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <NoteItem
+      v-for="note in notes"
+      :key="note.id"
+      :note="note"
+    />
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+import NoteItem from './NoteItem.vue';
+
+defineProps({
+  notes: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
