e0704915232aa5beab2b6ef05de3359a44148860
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-11-04 15:57:00 +0100

    added file

---
+++ src/components/modals/CreateNote.vue
@@ -0,0 +1,31 @@
+<template>
+  <fwb-modal
+    v-if="visible"
+    @close="$emit('update:visible', false)"
+    
+  >
+    <template #header>
+      <div class="flex items-center text-lg">
+        Create note
+      </div>
+    </template>
+    <template #body>
+      <form>
+        foifpooifkoi
+      </form>
+    </template>
+  </fwb-modal>
+</template>
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import { FwbModal } from 'flowbite-vue';
+
+defineProps({
+  visible: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+defineEmits(['update:visible']);
+</script>
